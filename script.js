e9fee8d2504921e8169308c47d4efcebc51795e7
7dvys <fdr160102@gmail.com>
2021-12-16 08:45:49 -0300

    nuevas cositas

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ window.onscroll = function() {
 
 
 // EXPERIENCE PANEL
-items = document.querySelectorAll('body main section#experience ul li.item');
-panels = document.querySelectorAll('body main section#experience .panel');
+let items = document.querySelectorAll('body main section#experience ul li.item');
+let panels = document.querySelectorAll('body main section#experience .panel');
 
 function experiencePanel(panel) {
 
@@ -49,33 +49,96 @@ function experiencePanel(panel) {
 }
 
 
-// LATEST PROJECT
-fetch('https://api.github.com/users/7dvys/repos')
-  .then(response => response.json())
-  .then(data => {
-    let currentProject=0;
-    var currentProjectUrl;
-    data.forEach(element => {
-      let compare = element.updated_at.localeCompare(currentProject)
+// GITHUB API
+fetch('https://api.github.com/users/7dvys/repos') //Get repos
+.then(response => response.json())
+.then(repos_info => {
+  let currentProject = 0;
+  let currentProjectUrl = '';
+  var repos_ready_to_display = [];
+  
+    repos_info.forEach(repo_info => {
+
+    // LATEST PROJECT
+      let compare = repo_info.updated_at.localeCompare(currentProject);
       if(compare == 1){
-        currentProject = element.updated_at;
-        console.log(element);
-        currentProjectUrl = element.clone_url;
+        currentProject = repo_info.updated_at;
+        currentProjectUrl = repo_info.clone_url;
       }
-    })
+
+      console.log(repo_info);
+
+    // Work I have built :D
+      let branch = 'master';
+      fetch(repo_info.contents_url.slice(0,-7)+'?ref='+branch) //Get repo content
+        .then(response => response.json())
+        .then(repo_content => {
+          if(repo_content.length != 0){
+            let portfolio_banner = false;
+            let readme = false;
+
+            repo_content.forEach(file => { //filter repos ready to display :3
+              if(file.name == 'portfolio_banner.png' || file.name == 'portfolio_banner.jpg' || file.name == 'portfolio_banner.jpeg'){
+                portfolio_banner = true;
+              }
+
+              if(file.name == 'readme.md'){
+                readme = true;
+              }
+
+              if(portfolio_banner != false && readme != false){  
+                repos_ready_to_display.pop(file);
+              }
+            })
+          }
+        })
+      })
+    
+    // Display Latest projext
     document.querySelector('a.current-project').href = currentProjectUrl;
+
   })
 
-// Work I have built :D
 
-  fetch('https://api.github.com/repos/7dvys/portfolio/contents/?ref=dev')
-    .then(response => response.json())
-    .then(data => {
-      fetch(data[1].download_url)
-        .then(response => response.text())
-        .then(data => {
-          var parser = new DOMParser();
-          var doc = parser.parseFromString(data,'text/html')
-        })
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // fetch('https://api.github.com/repos/7dvys/portfolio/contents/?ref=dev')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     fetch(data[1].download_url)
+  //       .then(response => response.text())
+  //       .then(data => {
+  //         var parser = new DOMParser();
+  //         var doc = parser.parseFromString(data,'text/html')
+  //       })
+  //   })
 
